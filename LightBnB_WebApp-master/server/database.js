567bfc99a6e560682d83f9324b81625e9dc0173d
Nolelle <eyu1@ualberta.ca>
2021-10-14 21:58:10 -0600

    Refactor addProperty function to pull from db

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -150,7 +150,7 @@ const getAllProperties = function (options, limit = 10) {
       queryString += `WHERE owner_id = $${queryParams.length} `;
     }
   }
-
+  //min price_per_night option
   if (options.minimum_price_per_night) {
     //price in db is in cents,convert to dollars
     queryParams.push(Number(options.minimum_price_per_night) * 100);
@@ -161,7 +161,7 @@ const getAllProperties = function (options, limit = 10) {
       queryString += `WHERE cost_per_night >= $${queryParams.length} `;
     }
   }
-
+  //max_price_per_night option
   if (options.maximum_price_per_night) {
     //price in db is in cents conver to dollars
     queryParams.push(Number(options.maximum_price_per_night) * 100);
@@ -196,9 +196,48 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  return pool
+    .query(
+      `
+      INSERT INTO properties (
+    owner_id,
+    title,
+    description,
+    thumbnail_photo_url,
+    cover_photo_url,
+    cost_per_night,
+    parking_spaces,
+    number_of_bathrooms,
+    number_of_bedrooms,
+    country,
+    street,
+    city,
+    province,
+    post_code) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+    RETURNING *;`,
+      [
+        property.owner_id,
+        property.title,
+        property.description,
+        property.thumbnail_photo_url,
+        property.cover_photo_url,
+        property.cost_per_night * 100,
+        property.parking_spaces,
+        property.number_of_bathrooms,
+        property.number_of_bedrooms,
+        property.country,
+        property.street,
+        property.city,
+        property.province,
+        property.post_code,
+      ]
+    )
+    .then((result) => {
+      // console.log(result);
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 exports.addProperty = addProperty;
